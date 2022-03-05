2c897e10ea665c49cb6d18e4a17547d2ade58888
Ruhita <ruhitapatil25@gmail.com>
2022-03-05 18:58:13 +0530

    Assessment phase 1 project

---
+++ Virtual key repository/src/com/project/lockedme/Main.java
@@ -0,0 +1,16 @@
+package com.project.lockedme;
+
+
+
+public class Main {
+	
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\RUHI\\eclipse-workspace\\Virtual key repository"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
